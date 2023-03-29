ff4f86d7e4d6e25d99961d39e77e4c95acb539df
alecasentini <ale.casentini@gmail.com>
2023-03-29 18:03:27 +0200

    Aggiunta Background-image + aggiunta buttons con function al click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,44 @@
 let thingsToBuy = [
-    'mele',
-    'arance',
-    'latte',
-    'insalata',
-    'patatine'
+    
 ];
 
-let i = 0
-let lista = document.querySelector('.lista')
-while (i<thingsToBuy.length){
+function add(){
+    let aggiunta = prompt ("aggiungi qualcosa alla lista");
+    thingsToBuy.push (aggiunta);
     
-    lista.innerHTML += `
-    <li>
-        ${thingsToBuy[i]}
-    </li>
-    `
-    i++;
+
+    let lista = document.querySelector('.lista');
+    lista.innerHTML = '';
+
+    let i = 0;
+
+    while (i<thingsToBuy.length){
+
+        lista.innerHTML += `
+        <li class="element">
+            ${thingsToBuy[i]}
+        </li>
+        `;
+
+        i++;
 }
+}
+
+function removeAll() {
+    thingsToBuy = [];
+    let lista = document.querySelector('.lista');
+    lista.innerHTML = '';
+}
+
+function removeLast() {
+    let lista = document.querySelector('.lista');
+    const lastElement = lista.lastElementChild;
+
+    if (lastElement) {
+      lista.removeChild(lastElement);
+    }
+  
+    if (thingsToBuy.length > 0) {
+      thingsToBuy.pop();
+    }
+  }
